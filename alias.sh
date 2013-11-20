fdeca3a5aec3d8914946cf344162b86d05a93b4f
bonzofenix <bonzofenix@gmail.com>
2013-11-20 14:52:18 -0300

    cf push

--- alias.sh
+++ alias.sh
@@ -2,3 +2,4 @@
 
 alias preview='open -a \"/Applications/Google\ Chrome.app/\"'
 alias doc='preview README.md'
+alias cfp='cf push | awk \"{ print $NF }\" | xargs open -a /Applications/Google\ Chrome.app'
